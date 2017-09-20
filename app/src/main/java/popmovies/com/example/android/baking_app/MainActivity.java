0545b5cf2291883d4ace7ba27d8fc6a6b3694ab2
Seth Watkins <seth.personal.smtp@gmail.com>
2017-09-20 07:43:14 -0500

    Init and implement RecipeLoader.

--- app/src/main/java/popmovies/com/example/android/baking_app/MainActivity.java
+++ app/src/main/java/popmovies/com/example/android/baking_app/MainActivity.java
@@ -1,35 +1,54 @@
 package popmovies.com.example.android.baking_app;
 
 import android.os.AsyncTask;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import popmovies.com.example.android.baking_app.data.Recipe;
+import popmovies.com.example.android.baking_app.loaders.RecipeLoader;
 import popmovies.com.example.android.baking_app.utils.JsonUtils;
 import popmovies.com.example.android.baking_app.utils.NetworkUtils;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String>{
+
+    public static final int RECIPE_LOADER = 123;
+
+    ArrayList<Recipe> recipes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new AsyncTask<Void, Void, String>() {
-            @Override
-            protected String doInBackground(Void... params) {
-               return NetworkUtils.getResponseFromURL(NetworkUtils.urlFromString(NetworkUtils.URL_FOR_JSON));
-            }
+        //Initialize the loader to fetch/parse the recipes.
+        LoaderManager loaderManager = getSupportLoaderManager();
+        loaderManager.initLoader(RECIPE_LOADER, null, this);
 
-            @Override
-            protected void onPostExecute(String s) {
-                TextView tv = (TextView) findViewById(R.id.only_text_view);
-                ArrayList<Recipe> r = JsonUtils.parseRecipes(s);
-                    tv.setText(r.get(0).getSteps().get(1 ).getShortDescription() + " " + r.get(1).getName() + " " + r.get(2).getName() + " " + r.get(3).getName());
-            }
-        }.execute();
+    }
+
+
+    @Override
+    public Loader<String> onCreateLoader(int id, Bundle args) {
+        if(id == RECIPE_LOADER) {
+            return new RecipeLoader(this);
+        }
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+        recipes = JsonUtils.parseRecipes(data);
+        Toast.makeText(this, "I parsed the data", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+
     }
 }
