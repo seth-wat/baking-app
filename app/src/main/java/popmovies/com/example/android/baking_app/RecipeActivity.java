e87139738514e00131e5f955bf938fc8ed8e678d
Seth Watkins <seth.personal.smtp@gmail.com>
2017-09-20 12:39:36 -0500

    Add RecipeFragment to the Activity.

--- app/src/main/java/popmovies/com/example/android/baking_app/RecipeActivity.java
+++ app/src/main/java/popmovies/com/example/android/baking_app/RecipeActivity.java
@@ -1,13 +1,42 @@
 package popmovies.com.example.android.baking_app;
 
+import android.os.Parcelable;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.Toast;
+
+import org.parceler.Parcel;
+import org.parceler.Parcels;
+
+import popmovies.com.example.android.baking_app.data.Recipe;
+import popmovies.com.example.android.baking_app.eventlisteners.MainRecipeClickListener;
+import popmovies.com.example.android.baking_app.fragments.RecipeFragment;
 
 public class RecipeActivity extends AppCompatActivity {
+    Recipe recipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        //Grab the recipe out of the intent.
+        if (getIntent().hasExtra(MainRecipeClickListener.RECIPE_EXTRA)) {
+            recipe = Parcels.unwrap((Parcelable) getIntent()
+                    .getExtras()
+                    .get(MainRecipeClickListener.RECIPE_EXTRA));
+        }
+
+        //Create a new recipe fragment and pass in the recipe we just retrieved.
+        RecipeFragment recipeFragment = new RecipeFragment();
+        recipeFragment.setRecipe(recipe);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.recipe_fragment_container, recipeFragment)
+                .commit();
     }
+
 }
