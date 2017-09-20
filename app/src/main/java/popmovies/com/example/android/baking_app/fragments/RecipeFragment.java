18805d228d520adbd6345c4fe0ac07ef4da8e97d
Seth Watkins <seth.personal.smtp@gmail.com>
2017-09-20 12:38:50 -0500

    Partial implementation of the Fragment that contains recipe/steps.

--- app/src/main/java/popmovies/com/example/android/baking_app/fragments/RecipeFragment.java
+++ app/src/main/java/popmovies/com/example/android/baking_app/fragments/RecipeFragment.java
@@ -3,107 +3,70 @@ package popmovies.com.example.android.baking_app.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import popmovies.com.example.android.baking_app.R;
+import popmovies.com.example.android.baking_app.data.Recipe;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link RecipeFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link RecipeFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * This fragment contains a list of ingredients and steps
+ * when a step is clicked it will pass data to the Fragment
+ * that displays step details.
  */
 public class RecipeFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    private Recipe recipe;
+    private onStepSelectedListener mCallback;
+    private Unbinder unbinder;
+
+    @BindView(R.id.recipe_name_text_view) TextView recipeNameTextView;
+    @BindView(R.id.ingredients_text_view) TextView ingredientsTextView;
+    @BindView(R.id.steps_recycler_view) RecyclerView stepsRecyclerView;
+
 
     public RecipeFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RecipeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RecipeFragment newInstance(String param1, String param2) {
-        RecipeFragment fragment = new RecipeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_recipe, container, false);
+        unbinder = ButterKnife.bind(this, rootView);
+
+        recipeNameTextView.setText(recipe.getName());
+        //Loop over the ingredient list and add them to the TextView.
+        for (int i = 0; i < recipe.getIngredients().size(); i++) {
+            ingredientsTextView.setText(ingredientsTextView.getText() + "\n" +
+                recipe.getIngredients().get(i).getIngredient());
         }
+        return rootView;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe, container, false);
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+    //check if the container activity implements this interface in onAttach
+    public interface onStepSelectedListener {
+        void onStepSelected();
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
+//    @Override
+//    public void onAttach(Context context) {
+//        try {
+//            mCallback = (onStepSelectedListener) context;
+//        } catch (ClassCastException e) {
+//
+//        }
+//    }
 }
