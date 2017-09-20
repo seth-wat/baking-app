aa5038a4909dc0badbae0bb76fee6edfb6d88cab
Seth Watkins <seth.personal.smtp@gmail.com>
2017-09-20 09:36:37 -0500

    onClickListener for each recipe that launched RecipeActivity

--- app/src/main/java/popmovies/com/example/android/baking_app/eventlisteners/MainRecipeClickListener.java
+++ app/src/main/java/popmovies/com/example/android/baking_app/eventlisteners/MainRecipeClickListener.java
@@ -1,8 +1,37 @@
 package popmovies.com.example.android.baking_app.eventlisteners;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Parcelable;
+import android.view.View;
+
+import org.parceler.Parcel;
+import org.parceler.Parcels;
+
+import popmovies.com.example.android.baking_app.MainActivity;
+import popmovies.com.example.android.baking_app.RecipeActivity;
+import popmovies.com.example.android.baking_app.data.Recipe;
+
 /**
- * Created by God on 9/20/2017.
+ * Handles clicks on recipes in MainActivity.
+ * Generates the intents to be passed into the fragments.
  */
 
-public class MainRecipeClickListener {
+public class MainRecipeClickListener implements View.OnClickListener {
+    public static final String RECIPE_EXTRA = "recipe_extra";
+    Recipe recipe;
+    Context context;
+
+    public MainRecipeClickListener(Recipe recipe, Context context) {
+        this.recipe = recipe;
+        this.context = context;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(context, RecipeActivity.class);
+        Parcelable recipeToPass = Parcels.wrap(recipe);
+        intent.putExtra(RECIPE_EXTRA, recipeToPass);
+        context.startActivity(intent);
+    }
 }
