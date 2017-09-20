825215f60a37e8f3e5cd0c2f03c939663f186f61
Seth Watkins <seth.personal.smtp@gmail.com>
2017-09-20 08:50:47 -0500

    Boilerplate for RecipieActiviy and corresponding Fragments.

---
+++ app/src/main/java/popmovies/com/example/android/baking_app/RecipeActivity.java
@@ -0,0 +1,13 @@
+package popmovies.com.example.android.baking_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+    }
+}
