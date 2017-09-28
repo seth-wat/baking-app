870a96074a190c09af49f03a3477e1613b02824d
Seth Watkins <seth.personal.smtp@gmail.com>
2017-09-28 14:03:14 -0500

    Adds the StepFragment to the RecipeActivity if a tablet is being used.

--- app/src/main/java/popmovies/com/example/android/baking_app/RecipeActivity.java
+++ app/src/main/java/popmovies/com/example/android/baking_app/RecipeActivity.java
@@ -17,6 +17,7 @@ import popmovies.com.example.android.baking_app.data.Recipe;
 import popmovies.com.example.android.baking_app.data.Step;
 import popmovies.com.example.android.baking_app.eventlisteners.MainRecipeClickListener;
 import popmovies.com.example.android.baking_app.fragments.RecipeFragment;
+import popmovies.com.example.android.baking_app.fragments.StepFragment;
 
 public class RecipeActivity extends AppCompatActivity implements
     RecipeFragment.onStepSelectedListener {
@@ -25,11 +26,18 @@ public class RecipeActivity extends AppCompatActivity implements
     public static final String EXTRA_STEP_POSITION = "extra_step_position";
     public static final String EXTRA_RECIPE_NAME = "extra_recipe_name";
     Recipe recipe;
+    boolean isTabletMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        /*
+        If step_fragment_container exists then we are in tablet layout
+        with the sw600dp.
+         */
+        if (findViewById(R.id.step_fragment_container) != null) isTabletMode = true;
 
         //Grab the recipe out of the intent.
         if (getIntent().hasExtra(MainRecipeClickListener.RECIPE_EXTRA)) {
@@ -38,16 +46,30 @@ public class RecipeActivity extends AppCompatActivity implements
                     .get(MainRecipeClickListener.RECIPE_EXTRA));
         }
         getSupportActionBar().setTitle(recipe.getName());
-        
+
         //Create a new recipe fragment and pass in the recipe we just retrieved.
         RecipeFragment recipeFragment = new RecipeFragment();
         recipeFragment.setRecipe(recipe);
         recipeFragment.setmCallback(this);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .add(R.id.recipe_fragment_container, recipeFragment)
-                .commit();
+        if (!isTabletMode) {
+            /*
+            If we are not in tablet mode then just add the RecipeFragment
+            to the Activity.
+             */
+            fragmentManager.beginTransaction()
+                    .add(R.id.recipe_fragment_container, recipeFragment)
+                    .commit();
+        } else {
+            //we are in tablet mode
+            StepFragment stepFragment = new StepFragment();
+            stepFragment.setStep(recipe.getSteps().get(0));
+            fragmentManager.beginTransaction()
+                    .add(R.id.recipe_fragment_container, recipeFragment)
+                    .add(R.id.step_fragment_container, stepFragment)
+                    .commit();
+        }
     }
 
     @Override
@@ -58,11 +80,23 @@ public class RecipeActivity extends AppCompatActivity implements
 
     @Override
     public void onStepSelected(List<Step> steps, int position) {
-        //Pass data to StepActivity
-        Intent intent = new Intent(this, StepActivity.class);
-        intent.putExtra(EXTRA_STEPS, Parcels.wrap(steps));
-        intent.putExtra(EXTRA_STEP_POSITION, position);
-        intent.putExtra(EXTRA_RECIPE_NAME, recipe.getName());
-        startActivity(intent);
+        if (!isTabletMode) {
+            //Pass data to StepActivity
+            Intent intent = new Intent(this, StepActivity.class);
+            intent.putExtra(EXTRA_STEPS, Parcels.wrap(steps));
+            intent.putExtra(EXTRA_STEP_POSITION, position);
+            intent.putExtra(EXTRA_RECIPE_NAME, recipe.getName());
+            startActivity(intent);
+        } else {
+            /*
+            We are in table mode and need to create a new StepFragment
+            to pass in the selected step and replace it in this Activity
+             */
+            StepFragment stepFragment = new StepFragment();
+            stepFragment.setStep(recipe.getSteps().get(position));
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.step_fragment_container, stepFragment)
+                    .commit();
+        }
     }
 }
